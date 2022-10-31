fcdb786b5f78a91409a1159ec00b1619b416b377
JosesFiles <115514401+JosesFiles@users.noreply.github.com>
2022-10-30 20:26:15 -0600

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+let age = 41;
+let country = "Mexico";
+let myname = "joseluis";
+let email = "airborne81gmail.com";
+let height = "5'4";
+let width = "5";
+let vacation = "paris";
+let drink = "coca cola";
+let whiskey = "johnny walker";
+let nickname = "Guero";
+let food = "Tacos";
+let military = "Army";
+let city = "Fresno";
+let hobbies = "jumping out of planes";
+let car = "toyota";
+let travel = "hawaii";
+let movie = "McFarland,USA";
+let highschoolsport = "Running";
+let mascot = "apache";
+let footballteam = "49ers";
+
+
+document.write ("<p>" + myname + " lives in "+ country + "</p>")
+
+document.write (`
+<p style = "color : blue"> name: ${myname}</p>
+<p style = "color : red"> Age: ${age}</p> 
+<p style = "color : green"> country: ${country}</p>
+<p style = "color : yellow"> email: ${email}</p>
+<p style = "color : orange"> height: ${height}</p>
+<p style = "color : purple"> width: ${width}</p>
+<p style = "color : aqua"> vacation: ${vacation}</p>
+<p style = "color : gray"> drink: ${drink}</p>
+<p style = "color : brown"> whiskey: ${whiskey}</p>
+<p style = "color : pink"> nickname: ${nickname}</p>
+<p style = "color : tomato"> food: ${food}</p>
+<p style = "color : maroon"> military: ${military}</p>
+<p style = "color : lime"> city: ${city}</p>
+<p style = "color : navy"> hobbies: ${hobbies}</p>
+<p style = "color : salmon"> car: ${car}</p>
+<p style = "color : chocolate"> travel: ${travel}</p>
+<p style = "color : blueviolet"> movie: ${movie}</p>
+<p styel = "color : turquoise"> highschoolsport: ${highschoolsport}</p>
+<p style = "color : Indigo"> mascot: ${mascot}</p>
+<p style = "color : violet"> footballteam: ${footballteam}</p>
+`);
